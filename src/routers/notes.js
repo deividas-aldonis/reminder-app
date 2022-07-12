60aee3b6d0193d8a563dbdd5acc212b483ff6355
deividas-aldonis <deividas.aldonis.dev@gmail.com>
2022-07-13 00:16:47 +0300

    a

--- src/routers/notes.js
+++ src/routers/notes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const schedule = require("node-schedule");
 const sendMail = require("../modules/email");
 
 router.post("/test", (req, res) => {
-  sendMail("deividasaldo76@gmail.com");
+  //   sendMail("deividasaldo76@gmail.com");
+  // const job = schedule.scheduleJob()
+  console.log(req.body.date);
+
+  const d = req.body.date;
 
   res.send("");
 });
